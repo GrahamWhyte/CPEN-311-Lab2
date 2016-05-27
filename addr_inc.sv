7d15016791fabe709e11175ab2937be1059e14d8
GrahamWhyte <grahamwhyte25@gmail.com>
2016-05-27 14:37:10 -0700

    Fixed clock

--- addr_inc.sv
+++ addr_inc.sv
@@ -7,9 +7,9 @@ Outputs: Address and bytes to read from
 		 Start signal for flash and end signal 
 */
 
-module addr_inc(clk, start, change, endFlash, address, byteenable, startFlash, finish, songData, outData); 
+module addr_inc(clk, audioClk, start, change, endFlash, address, byteenable, startFlash, finish, songData, outData); 
 
-input clk, start, endFlash;
+input clk, audioClk, start, endFlash;
 input [1:0] change;  
 input [31:0] songData; 
 output startFlash, finish; 
@@ -24,6 +24,7 @@ parameter inc_addr = 5'b011_00;
 parameter dec_addr = 5'b100_00; 
 parameter finished = 5'b101_10; 
 parameter get_data = 5'b110_00; 
+parameter read_data = 5'b111_00; 
 
 logic [4:0] state; 
 logic flag; 
@@ -50,7 +51,11 @@ always_ff@(posedge clk) begin
 				   if(endFlash) state <= get_data; 
 				   end 
 				   
-		get_data: state <= checkInc; 
+		get_data: begin 
+				  if (audioClk) state <= read_data;
+				  end 
+				  
+		read_data: state <= checkInc; 
 				   
 		checkInc: begin 
 				  if(dir) state <= dec_addr; 
@@ -75,15 +80,15 @@ end
 //Output logic 
 always_ff@(posedge clk) begin 
 	
-	if (state == get_data) begin 
-		if (flag) begin 
+	if (state == read_data) begin 
+		if (flag) begin
 			outData = songData [31:16]; 
-			flag = ~flag; 
-			end 
+			flag = ~flag;
+		end 
 		else begin 
 			outData = songData [15:0]; 
-			flag = ~flag; 
-			end 
+			flag = ~flag;  
+		end 
 	end 
 	
 	else if (state == dec_addr) begin 
