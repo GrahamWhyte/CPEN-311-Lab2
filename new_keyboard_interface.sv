dc97898ae1841ace4bd5352be0ef12d4803ced94
GrahamWhyte <grahamwhyte25@gmail.com>
2016-06-05 15:35:44 -0700

    Added comments

--- new_keyboard_interface.sv
+++ new_keyboard_interface.sv
@@ -1,3 +1,13 @@
+/*
+Purpose: Control flash memory reads with keyboard 
+
+Inputs from top module: 50 MHz clock, Last key pressed, Key pressed signal
+Inputs from address increment: read complete signal 
+
+Outputs to address increment: start signal, direction to count, restart signal 
+*/ 
+
+
 `define character_B 8'h42  
 `define character_D 8'h44 
 `define character_E 8'h45 
@@ -9,8 +19,6 @@
 `define character_lowercase_f 8'h66
 `define character_lowercase_r 8'h72 
 
-/*This FSM module decides the read_mem, direction, and reset values to feed into
-address FSM based on the keyboard inputs*/
 module new_keyboard_interface(input logic clk, 
 							input logic [7:0] key, 
 							input readFinish,
@@ -19,45 +27,22 @@ module new_keyboard_interface(input logic clk,
 							output logic dir,
 							output logic restart); 
 	
-	/*States that keyboard presses transition between*/
-	/********outputs encoded in state bits (greycode):
-			State[2]	State[1]	State[0]
-			dir[1]		dir[0]		start_read
-			direction	reset
-	*/
-	
+	//State encodings 
 	parameter check_key = 6'b000_000; 
 	parameter Foreward = 6'b001_001;
 	parameter Foreward_reset = 6'b010_101;
 	parameter Foreward_pause = 6'b011_000;
-	
 	parameter Backward = 6'b100_011;
 	parameter Backward_reset = 6'b101_111;
 	parameter Backward_pause = 6'b110_000;
 	
-	/*TO BE DELETED: names assigned to dir[1:0] output for clarity
-	 *basically the same as setting dir to upper two bits of current state*/
-	 
-	/*parameter read_forward = 2'b00;	
-	parameter reset_forward = 2'b01;
-	parameter read_backward = 2'b10;
-	parameter reset_backward = 2'b11;*/
-
+	//Outputs dependant on lower bits of state encoding 
 	assign restart = state[2]; 
 	assign dir = state[1]; 
 	assign start_read = state[0]; 
 	
 	logic [5:0] state; 
-	//logic [5:0] next_state;
-	
-	/*state register (reset handled in comb block)*/
-	/*always_ff @(posedge clk)
-		state <= next_state;
-	*/
-	/*next state logic*/
-	//always_comb begin
-	 
-	
+
 	always_ff@(posedge clk) begin
 		case(state)
 		
@@ -70,12 +55,13 @@ module new_keyboard_interface(input logic clk,
 			
 			
 			Foreward_reset: begin 
-							if (readFinish) state <= Foreward; 
+							if (readFinish) 
+								state <= Foreward; //Wait for complete read after resetting 
 							end 
 			
 			Foreward: begin 
 					if(key == `character_R || key == `character_lowercase_r) begin 
-						if(dataReady) state <= Foreward_reset;
+						if(dataReady) state <= Foreward_reset; //Only reset when key is pressed 
 						else state <= Foreward; 
 					end 
 					else if(key == `character_D || key == `character_lowercase_d)
@@ -86,19 +72,22 @@ module new_keyboard_interface(input logic clk,
 				end 
 					
 			Foreward_pause: begin 
-				if(key == `character_R || key == `character_lowercase_r)
-					state <= Foreward_reset;
+				if(key == `character_R || key == `character_lowercase_r) begin 
+					state <= Foreward_reset; 
+				end 
 				else if(key == `character_E || key == `character_lowercase_e)
 					state <= Foreward;
+				else if(key == `character_B || key == `character_lowercase_b) 
+					state <= Backward_pause; 
 				else state <= Foreward_pause; 
 				end 
 				
-			Backward_reset: if (readFinish) state <= Backward;
+			Backward_reset: if (readFinish) state <= Backward; //Wait for complete read after resetting 
 		
 			
 			Backward: begin 
 					if(key == `character_R || key == `character_lowercase_r) begin 
-						if (dataReady) state <= Backward_reset;
+						if (dataReady) state <= Backward_reset;  //Only reset when key is pressed 
 						else state <= Backward; 
 					end 
 				    else if(key == `character_D || key == `character_lowercase_d)
@@ -110,10 +99,13 @@ module new_keyboard_interface(input logic clk,
 				end 
 				
 			Backward_pause: begin 
-				if(key == `character_R || key == `character_lowercase_r)
-					state <= Backward_reset;
+				if(key == `character_R || key == `character_lowercase_r) begin 
+					state <= Backward_reset; 
+				end 
 				else if(key == `character_E || key == `character_lowercase_e)
 					state <= Backward;
+				else if(key == `character_F || key == `character_lowercase_f) 
+					state <= Foreward_pause; 
 				else
 					state <= Backward_pause;	
 				end 
@@ -121,8 +113,5 @@ module new_keyboard_interface(input logic clk,
 			default: state <= check_key; 
 		endcase
 	end
-	
-	
-	
 endmodule
 						
